8c432f8b78b7e74c3fabfa0383cabff7a678a457
jash413 <99473238+jash413@users.noreply.github.com>
2024-11-05 11:04:45 +0530

    FIX: JOB-CATE-ROUTES

--- routes/jobCateRoutes.js
+++ routes/jobCateRoutes.js
@@ -7,7 +7,7 @@ const authMiddleware = require("../middlewares/authMiddleware");
 // Get all job categories
 router.post(
   "/get-job-categories",
-  authMiddleware(["AMN"]),
+  authMiddleware(["AMN","CAN"]),
   jobCateController.getAllJobCategories
 );
 
